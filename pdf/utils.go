5978085d87e44225b51bc0713bc55a2ce479ed0b
Gunnsteinn Hall <ghall@GHFoxy-Mac-mini.local>
2016-11-28 22:21:20 +0000

    Updated object types to check for (javascript and videos)

--- pdf/utils.go
+++ pdf/utils.go
@@ -62,9 +62,8 @@ func (this *PdfParser) inspect() (map[string]int, error) {
 			common.Log.Debug("IND OOBJ %d: %s", xref.objectNumber, iobj)
 			dict, isDict := iobj.PdfObject.(*PdfObjectDictionary)
 			if isDict {
-				ot, ok := (*dict)["Type"].(*PdfObjectName)
-
-				if ok {
+				// Check if has Type parameter.
+				if ot, has := (*dict)["Type"].(*PdfObjectName); has {
 					otype := string(*ot)
 					common.Log.Debug("---> Obj type: %s", otype)
 					_, isDefined := objTypes[otype]
@@ -73,7 +72,27 @@ func (this *PdfParser) inspect() (map[string]int, error) {
 					} else {
 						objTypes[otype] = 1
 					}
+				} else if ot, has := (*dict)["Subtype"].(*PdfObjectName); has {
+					// Check if subtype
+					otype := string(*ot)
+					common.Log.Debug("---> Obj subtype: %s", otype)
+					_, isDefined := objTypes[otype]
+					if isDefined {
+						objTypes[otype]++
+					} else {
+						objTypes[otype] = 1
+					}
 				}
+				if val, has := (*dict)["S"].(*PdfObjectName); has && *val == "JavaScript" {
+					// Check if Javascript.
+					_, isDefined := objTypes["JavaScript"]
+					if isDefined {
+						objTypes["JavaScript"]++
+					} else {
+						objTypes["JavaScript"] = 1
+					}
+				}
+
 			}
 		} else if sobj, isStream := o.(*PdfObjectStream); isStream {
 			if otype, ok := (*(sobj.PdfObjectDictionary))["Type"].(*PdfObjectName); ok {
