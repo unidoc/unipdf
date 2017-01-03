d33cdf1c5c447ef533d4b5cd9646071ef50518b0
Gunnsteinn Hall <ghall@GHFoxy-Mac-mini.lan>
2017-01-03 15:24:54 +0000

    Update versioning

--- common/version.go
+++ common/version.go
@@ -10,14 +10,14 @@ import (
 	"time"
 )
 
-const releaseYear = 2016
-const releaseMonth = 7
-const releaseDay = 9
-const releaseHour = 12
-const releaseMin = 00
+const releaseYear = 2017
+const releaseMonth = 1
+const releaseDay = 3
+const releaseHour = 15
+const releaseMin = 0
 
 // Holds version information, when bumping this make sure to bump the released
 // at stamp also. This is for license information to make sure license
-const Version = "1.0.0"
+const Version = "2.0.0-alpha.1"
 
 var ReleasedAt = time.Date(releaseYear, releaseMonth, releaseDay, releaseHour, releaseMin, 0, 0, time.UTC)
