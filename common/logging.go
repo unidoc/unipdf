b8f5d4879fcaaa289fb1e607f7f9211722234603
Peter Williams <peter.williams@papercut.com>
2017-01-12 17:52:29 +1100

    Added file:line to log messages

--- common/logging.go
+++ common/logging.go
@@ -7,6 +7,8 @@ package common
 
 import (
 	"fmt"
+	"path/filepath"
+	"runtime"
 )
 
 type Logger interface {
@@ -39,32 +41,40 @@ func (this DummyLogger) Debug(format string, args ...interface{}) {
 type ConsoleLogger struct{}
 
 func (this ConsoleLogger) Error(format string, args ...interface{}) {
-	prefix := "[ERROR] "
-	fmt.Printf(prefix+format+"\n", args...)
+	this.output("[ERROR] ", format, args...)
 }
 
 func (this ConsoleLogger) Warning(format string, args ...interface{}) {
-	prefix := "[WARNING] "
-	fmt.Printf(prefix+format+"\n", args...)
+	this.output("[WARNING] ", format, args...)
 }
 
 func (this ConsoleLogger) Notice(format string, args ...interface{}) {
-	prefix := "[NOTICE] "
-	fmt.Printf(prefix+format+"\n", args...)
+	this.output("[NOTICE] ", format, args...)
 }
 
 func (this ConsoleLogger) Info(format string, args ...interface{}) {
-	prefix := "[INFO] "
-	fmt.Printf(prefix+format+"\n", args...)
+	this.output("[INFO] ", format, args...)
 }
 
 func (this ConsoleLogger) Debug(format string, args ...interface{}) {
-	prefix := "[DEBUG] "
-	fmt.Printf(prefix+format+"\n", args...)
+	this.output("[DEBUG] ", format, args...)
+}
+
+func (this ConsoleLogger) output(prefix, format string, args ...interface{}) {
+	_, file, line, ok := runtime.Caller(3)
+	if !ok {
+		file = "???"
+		line = 0
+	} else {
+		file = filepath.Base(file)
+	}
+	src := fmt.Sprintf("%s:%d %s", file, line, prefix) + format + "\n"
+	fmt.Printf(src, args...)
 }
 
 var Log Logger = DummyLogger{}
 
 func SetLogger(logger Logger) {
 	Log = logger
+	fmt.Printf("SetLogger: logger=%+v\n", logger)
 }
