206dccb560435f5ab565af8aa1cb8656c8149b6d
Gunnsteinn Hall <ghall@GHFoxy-Mac-mini.local>
2017-07-17 20:48:02 +0000

    Release v2.0.0-rc.1

--- common/version.go
+++ common/version.go
@@ -12,11 +12,11 @@ import (
 
 const releaseYear = 2017
 const releaseMonth = 7
-const releaseDay = 7
-const releaseHour = 17
-const releaseMin = 20
+const releaseDay = 17
+const releaseHour = 20
+const releaseMin = 45
 
 // Holds version information, when bumping this make sure to bump the released at stamp also.
-const Version = "2.0.0-alpha.5"
+const Version = "2.0.0-rc.1"
 
 var ReleasedAt = time.Date(releaseYear, releaseMonth, releaseDay, releaseHour, releaseMin, 0, 0, time.UTC)
