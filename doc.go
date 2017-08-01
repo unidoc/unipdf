5d3a6d9ab98bd24e4a6747bd634fcf15514e3570
Gunnsteinn Hall <ghall@GHFoxy-Mac-mini.lan>
2017-08-01 15:04:06 +0000

    Fix godoc code block line wrapping

--- doc.go
+++ doc.go
@@ -7,7 +7,7 @@
 // processing and modifying PDFs. UniDoc is written and supported by the owners of the
 // FoxyUtils.com website, where the library is used to power many of the PDF services offered.
 //
-// Getting more information
+// Getting More Information
 //
 // Check out the Getting Started and Example sections, which showcase how to install UniDoc and provide numerous
 // examples of using UniDoc to generate, process or modify PDF files.
@@ -16,22 +16,24 @@
 // The godoc for unidoc provides a detailed breakdown of the API and documentation for packages, types and methods.
 // https://godoc.org/github.com/unidoc/unidoc
 //
-// Overview of the API's major packages
+// Overview of Major Packages
 //
 // The API is composed of a few major packages:
 //
-//   - unidoc/common: Provides common shared types such as Logger and utilities to check license.
+//   - common: Provides common shared types such as Logger and utilities to check license.
 //
-//   - unidoc/pdf/core: The core package defines the primitive PDF object types and handles the file reading I/O and
-//     parsing the primitive objects.
+//   - pdf/core: The core package defines the primitive PDF object types and handles the
+//     file reading I/O and parsing the primitive objects.
 //
-//   - unidoc/pdf/model: The model package builds on the core package, to represent the PDF as a structured model of the
-//     PDF primitive types. It has a reader and a writer to read and process a PDF file based on the structured model.
-//     This serves as a basis to perform a number of numerous tasks and can be used to work with a PDF in a medium to
-//     high level interface, although it does require an understanding of the PDF format and structure.
+//   - pdf/model: The model package builds on the core package, to represent the PDF as a
+//     structured model of the PDF primitive types. It has a reader and a writer to read and
+//     process a PDF file based on the structured model. This serves as a basis to perform a
+//     number of numerous tasks and can be used to work with a PDF in a medium to high level
+//     interface, although it does require an understanding of the PDF format and structure.
 //
-//   - unidoc/pdf/creator: The PDF creator makes it easy to create new PDFs or modify existing PDFs. It can also enable
-//     loading a template PDF, adding text/images and generating an output PDF. It can be used to add text, images,
-//     and generate text and graphical reports. It is designed with simplicity in mind, with the goal of making it easy
-//     to create reports without needing any knowledge about the PDF format or specifications.
+//   - pdf/creator: The PDF creator makes it easy to create new PDFs or modify existing PDFs.
+//     It can also enable loading a template PDF, adding text/images and generating an output
+//     PDF. It can be used to add text, images, and generate text and graphical reports. It
+//     is designed with simplicity in mind, with the goal of making it easy to create reports
+//     without needing any knowledge about the PDF format or specifications.
 package unidoc
