bf5496aa8dde1cd4638365452694b6d68508e35a
qz <qz@pandaren.local>
2018-04-11 21:47:48 +0300

    page Resources can be loaded from indirect object

--- pdf/model/page.go
+++ pdf/model/page.go
@@ -419,6 +419,8 @@ func (this *PdfPage) getResources() (*PdfPageResources, error) {
 		}
 
 		if obj := dict.Get("Resources"); obj != nil {
+			obj = TraceToDirectObject(obj)
+
 			prDict, ok := obj.(*PdfObjectDictionary)
 			if !ok {
 				return nil, errors.New("Invalid resource dict!")
