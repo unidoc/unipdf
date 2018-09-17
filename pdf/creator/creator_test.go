eb6ffda9fa974004b1c0b4883be3b7f24f4ffa24
Peter Williams <peter.williams@papercut.com>
2018-09-18 08:06:12 +1000

    added a comment

--- pdf/creator/creator_test.go
+++ pdf/creator/creator_test.go
@@ -37,6 +37,10 @@ const testImageFile2 = "./testdata/signature.png"
 const testRobotoRegularTTFFile = "./testdata/roboto/Roboto-Regular.ttf"
 const testRobotoBoldTTFFile = "./testdata/roboto/Roboto-Bold.ttf"
 const testWts11TTFFile = "./testdata/wts11.ttf"
+
+// XXX: /tmp/2_p_multi.pdf which is created in this test gives an error message when opened in
+//      Adobe Reader: The font FreeSans contains bad Widths.
+//      This problem did not occur when I replaced FreeSans.ttf with LiberationSans-Regular.ttf
 const testFreeSansTTFFile = "./testdata/FreeSans.ttf"
 
 func TestTemplate1(t *testing.T) {
