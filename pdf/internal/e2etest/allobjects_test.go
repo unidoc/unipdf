e4d4f68e2bb58191e2632efddc2c9691e313a170
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2019-01-23 10:00:24 +0000

    Cleanup headers and whitespace

--- pdf/internal/e2etest/allobjects_test.go
+++ pdf/internal/e2etest/allobjects_test.go
@@ -1,3 +1,8 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.md', which is part of this source code package.
+ */
+
 package e2etest
 
 import (
@@ -8,7 +13,6 @@ import (
 	"testing"
 
 	"github.com/unidoc/unidoc/pdf/core"
-
 	"github.com/unidoc/unidoc/pdf/model"
 )
 
