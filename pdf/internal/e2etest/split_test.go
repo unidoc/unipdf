7d03f86a33bd2be99b6c51ca500511b1b09cc46a
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2019-04-21 00:56:21 +0000

    Make e2e split test version independent

--- pdf/internal/e2etest/split_test.go
+++ pdf/internal/e2etest/split_test.go
@@ -30,15 +30,15 @@ var (
 // knownHashes defines a list of known output hashes to ensure that the output is constant.
 // If there is a change in hash need to find out why and update only if the change is accepted.
 var knownHashes = map[string]string{
-	"bf7c9d5dabc7e7ec2fc0cf9db2d9c8e7aa456fca.pdf": "0858340ec31e869d7e65a67f5e7b97f5",
-	"371dce2c2720581a3eef3f123e5741dd3566ef87.pdf": "2343e102e15c5a4ac6ea8e95dab032cc",
-	"e815311526b50036db6e89c54af2b9626edecf30.pdf": "b3843bf3f9df85040df075646e5ffc13",
-	"3bf64014e0c9e4a56f1a9363f1b34fd707bd9fa0.pdf": "3ad89699968b30a31c00a0480ebd9b73",
-	"004feecd47e2da4f2ed5cdbbf4791a77dd59ce20.pdf": "74137fe68bfce5135af4ff55dbd471f1",
-	"30c0a5cff80870cd58c2738d622f5d63e37dc90c.pdf": "d1a25c9f5dd1cfc2b665eb4999a1135c",
-	"8f8ce400b9d66656cd09260035aa0cc3f7e46c82.pdf": "c94cd56024c724b370ecc61fb67b0fa5",
-	"a35d386af4828b7221591343761191e8f9a28bc0.pdf": "c8858dffbfc091fbb4e32b26bfb33bde",
-	"e815699a5234540fda89ea3a2ece055349a0d535.pdf": "024763e941869dd195438277fa187c30",
+	"bf7c9d5dabc7e7ec2fc0cf9db2d9c8e7aa456fca.pdf": "f7891d491fa9f20ed2975dd28961c205",
+	"371dce2c2720581a3eef3f123e5741dd3566ef87.pdf": "4a25934226b6b64e5d95d571260b1f01",
+	"e815311526b50036db6e89c54af2b9626edecf30.pdf": "97dcfdde59a2f3a6eb105d0c31ebd3fb",
+	"3bf64014e0c9e4a56f1a9363f1b34fd707bd9fa0.pdf": "6f310c9fdd44d49766d3cc32d3053b89",
+	"004feecd47e2da4f2ed5cdbbf4791a77dd59ce20.pdf": "309a072a97d0566aa3f85edae504bb53",
+	"30c0a5cff80870cd58c2738d622f5d63e37dc90c.pdf": "67d7c2fbf21dd9d65c8bb9ab29dfec60",
+	"8f8ce400b9d66656cd09260035aa0cc3f7e46c82.pdf": "679650c27697a7b83ee792692daaff18",
+	"a35d386af4828b7221591343761191e8f9a28bc0.pdf": "1955d6cf29715652bea999bcbadc818b",
+	"e815699a5234540fda89ea3a2ece055349a0d535.pdf": "5a1d97ee1aabc5dcacbbf3cd164b964d",
 }
 
 func TestSplitting(t *testing.T) {
@@ -126,6 +126,7 @@ func splitSinglePdf(t *testing.T, params splitParams) {
 		return
 	}
 
+	model.SetPdfProducer("UniDoc")
 	writer := model.NewPdfWriter()
 
 	// Split the first page.
