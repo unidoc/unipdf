ebc1cb11bb63f97ca1d7e6278fe77a10bd024a52
Alfred Hall <ahall@ahall.org>
2019-06-07 14:07:57 +0000

    License handling, expiry

--- common/version.go
+++ common/version.go
@@ -12,11 +12,11 @@ import (
 
 const releaseYear = 2019
 const releaseMonth = 6
-const releaseDay = 2
+const releaseDay = 7
 const releaseHour = 11
 const releaseMin = 30
 
 // Version holds version information, when bumping this make sure to bump the released at stamp also.
-const Version = "3.0.1"
+const Version = "3.0.2"
 
 var ReleasedAt = time.Date(releaseYear, releaseMonth, releaseDay, releaseHour, releaseMin, 0, 0, time.UTC)
