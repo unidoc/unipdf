23f1f9242afc1deef53c76e5ace7e041f06810d8
Gunnsteinn Hall <gunnsteinn.hall@gmail.com>
2019-07-23 20:10:39 +0000

    Simplify lazy loading logic, remove redundancy. Fixes, improved performance.

--- model/page.go
+++ model/page.go
@@ -736,7 +736,7 @@ func (p *PdfPage) AddContentStreamByString(contentStr string) error {
 	if p.Contents == nil {
 		// If not set, place it directly.
 		p.Contents = stream
-	} else if contArray, isArray := core.TraceToDirectObject(p.Contents).(*core.PdfObjectArray); isArray {
+	} else if contArray, isArray := core.GetArray(p.Contents); isArray {
 		// If an array of content streams, append it.
 		contArray.Append(stream)
 	} else {
