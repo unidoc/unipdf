2b65743f8735a6c39bb70deea493dd6ea6483d9b
Adrian-George Bostan <adrg@epistack.com>
2019-09-02 22:30:54 +0300

    Improve Travis build_examples script

--- .travis/build_examples.sh
+++ .travis/build_examples.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-repo_path="$GOPATH/src/github.com/unidoc"
-mkdir -p $repo_path
+repo_path="$GOPATH/src/github.com/unidoc/unipdf-examples"
+git clone https://github.com/unidoc/unipdf-examples.git $repo_path
 cd $repo_path
-git clone https://github.com/unidoc/unipdf-examples.git
-cd unipdf-examples
 
 branch_name="v3"
-if [ `git branch --list "$TRAVIS_BRANCH"` ]
-then
+if [[ `git ls-remote origin "$TRAVIS_BRANCH"` ]]; then
     branch_name="$TRAVIS_BRANCH"
 fi
 
