065f0136372f5a2d285202ce0c66598269884468
Adrian-George Bostan <adrg@epistack.com>
2019-10-28 21:47:46 +0200

    Take page cropbox into account

--- render/renderer.go
+++ render/renderer.go
@@ -26,16 +26,21 @@ func (r renderer) renderPage(page *model.PdfPage, ctx context.Context) error {
 		return err
 	}
 
-	// Create white background.
-	ctx.Push()
-	ctx.SetRGBA(1, 1, 1, 1)
-	ctx.DrawRectangle(0, 0, float64(ctx.Width()), float64(ctx.Height()))
-	ctx.Fill()
-	ctx.Pop()
-
 	// Change coordinate system.
 	ctx.Translate(0, float64(ctx.Height()))
 	ctx.Scale(1, -1)
+
+	var x, y, w, h float64 = 0, 0, float64(ctx.Width()), float64(ctx.Height())
+	if box := page.CropBox; box != nil {
+		x, y, w, h = box.Llx, box.Lly, box.Urx-box.Llx, box.Ury-box.Lly
+	}
+
+	// Create white background.
+	ctx.Push()
+	ctx.SetRGBA(1, 1, 1, 1)
+	ctx.DrawRectangle(x, y, w, h)
+	ctx.Fill()
+	ctx.Pop()
 
 	// Set defaults.
 	ctx.SetLineWidth(1.0)
