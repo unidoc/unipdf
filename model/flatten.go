c38ed1a2b87fc08fda8247621a257a43694c97ed
Lanre Ade <adebambo@ualberta.ca>
2020-03-03 18:55:55 -0700

    fix: check appgen variable not nil before WrapContentStream

--- model/flatten.go
+++ model/flatten.go
@@ -85,9 +85,11 @@ func (r *PdfReader) FlattenFields(allannots bool, appgen FieldAppearanceGenerato
 		var annots []*PdfAnnotation
 
 		// Wrap the content streams.
-		err := appgen.WrapContentStream(page)
-		if err != nil {
-			return err
+		if appgen != nil {
+			err := appgen.WrapContentStream(page)
+			if err != nil {
+				return err
+			}
 		}
 
 		annotations, err := page.GetAnnotations()
