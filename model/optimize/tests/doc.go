386829f2bbe54d91d5120a70e404eab7d9c57222
Jacek Kucharczyk <kucjac@gmail.com>
2020-04-07 14:41:47 +0200

    Added doc file for the optimize/tests package.

---
+++ model/optimize/tests/doc.go
@@ -0,0 +1,8 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.md', which is part of this source code package.
+ */
+
+// Package tests provides integration tests for the UniPDF optimizer. The test requires environment variable
+// UNIDOC_OPTIMIZE_TESTDATA to be set for the directory with `.pdf` files, otherwise the test is skipped.
+package tests
